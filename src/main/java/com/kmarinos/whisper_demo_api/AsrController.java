132a4a14bf64cf6f78479254c216375a24a147e4
Criux <greekmule@gmail.com>
2025-10-05 16:21:35 +0200

    init

---
+++ src/main/java/com/kmarinos/whisper_demo_api/AsrController.java
@@ -0,0 +1,53 @@
+package com.kmarinos.whisper_demo_api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("api/v1")
+public class AsrController {
+
+  @GetMapping("health")
+  public ResponseEntity<Void> health(){
+    return ResponseEntity.ok().build();
+  }
+  @PostMapping("asr")
+  public ResponseEntity<String>asr(HttpServletRequest request,@RequestHeader HttpHeaders headers, @RequestParam Map<String,String> params, @RequestPart("audio_file")
+      MultipartFile file){
+    String queryString = request.getQueryString();
+
+    RestTemplate restTemplate = new RestTemplate();
+    String response = forwardRequest(headers, file, queryString);
+    System.out.println("Response:"+response);
+    return ResponseEntity.ok(response);
+  }
+  private String forwardRequest(HttpHeaders headers, MultipartFile file, String queryString){
+    MultiValueMap<String, Object> body
+        = new LinkedMultiValueMap<>();
+    body.add("audio_file",file.getResource());
+    HttpEntity<MultiValueMap<String,Object>> requestEntity
+        = new HttpEntity<>(body, headers);
+
+    String serverUrl = "http://app-prd01.marinos.com:9010/asr?"+queryString;
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<String> response = restTemplate
+        .postForEntity(serverUrl, body, String.class);
+    return response.getBody();
+  }
+}
---
+++ src/main/java/com/kmarinos/whisper_demo_api/WhisperDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kmarinos.whisper_demo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhisperDemoApiApplication.class, args);
+	}
+
+}
